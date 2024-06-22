3be8d53e01fac30f1f0040fb63b32d947e4701fa
jaikrishnavr01 <jaikrishnavr@outlook.com>
2024-06-22 12:42:45 +0530

    server created

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-import express from "express";
+
+import http from "node:http"
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const server = http.createServer((req,res) => {
+    res.writeHead(200,{"content-type": "text/plain"})
+    res.end("Hello")
+})
+
+server.listen(process.env.PORT, () => {
+    console.log(`server is running on port ${process.env.PORT}`);
+})
